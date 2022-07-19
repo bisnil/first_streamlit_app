6cebcacedd661caaa3468a0cbc7accf561d283c7
bisnil <71563041+bisnil@users.noreply.github.com>
2022-07-19 17:54:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍞 Breakfast Favourites 🍞')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal 🥣')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie 🥗')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg 🐔')
+streamlit.text('🥑 Avocado Toast 🥑')
+   
