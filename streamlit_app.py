3f4a59c9a10c962bfa0d190b29943fa4344f90c1
bisnil <71563041+bisnil@users.noreply.github.com>
2022-07-19 17:49:11 +0530

    Update streamlit_app.py
    Fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents Healthy Diner')
-
 streamlit.header('Breakfast Menu')
-
-streamlit.text('Omega 3 and Blueberry Oatsmeal')
-
-streamlit.text('Kale Spinach and Rocket Smoothie')
-
-streamlit.text('Hard Bioled Free range Eggs')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
