f19f2b696fd4663680e08f97b2c456f94bf70961
bisnil <71563041+bisnil@users.noreply.github.com>
2022-07-19 17:47:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My Parents Healthy Diner')
 
 streamlit.subtitle('Breakfast Menu')
+
 streamlit.text('Omega 3 and Blueberry Oatsmeal')
+
 streamlit.text('Kale Spinach and Rocket Smoothie')
+
 streamlit.text('Hard Bioled Free range Eggs')
