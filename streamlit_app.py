bca33fb524b0394f32e527057718d51d7d7062ec
bisnil <71563041+bisnil@users.noreply.github.com>
2022-07-19 17:46:53 +0530

    Update streamlit_app.py
    Added Subtitle and breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents Healthy Diner')
 
-streamlit.text('Breakfast Menu')
+streamlit.subtitle('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatsmeal')
+streamlit.text('Kale Spinach and Rocket Smoothie')
+streamlit.text('Hard Bioled Free range Eggs')
